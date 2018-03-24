ed841285d20e0c96d21b5825915fb659b586ef35
Simulator-PC\Xingbang <yangsib1987@gmail.com>
2018-03-24 17:46:04 -0400

    add force sensor and exo structure parameters

--- RunningExo/inc/user-mn-RunningExo.h
+++ RunningExo/inc/user-mn-RunningExo.h
@@ -21,6 +21,7 @@
 // Include(s)
 //****************************************************************************
 #include "main.h"
+#include "math.h"
 
 //****************************************************************************
 // Public Function Prototype(s):
@@ -116,6 +117,77 @@ void stateTransition(void);
 //****************************************************************************
 // Shared variable(s)
 //****************************************************************************
+
+
+
+//****************************************************************************
+// Force sensor parameter(s)
+//****************************************************************************
+#if ACTIVE_LEG == LEFT_ANKLE
+
+#define LOAD_MAX_IN_POUND				500		// lb, maximum mass the force sensor can measure
+#define KILOGRAM_PER_POUNUD				0.4535924
+#define LOAD_MAX_IN_KILOGRAM			LOAD_MAX_IN_POUND*KILOGRAM_PER_POUNUD		// 226.7962 kg, maximum mass the force sensor can measure
+#define GRAVITY_ACC						9.8
+#define FORCE_MAX						LOAD_MAX_IN_KILOGRAM*GRAVITY_ACC		// 2222.602613 Newton,  for LCM200 load cell maximum force the force sensor can measure
+#define FORCE_STRAIN_SENSITIVITY		2.1938 	// mV/V, force sensor sensitivity
+#define FORCE_EXCIT						5		// V, Excitation Voltage
+#define FULL_RANGE_OUTPUT				FORCE_STRAIN_SENSITIVITY*FORCE_EXCIT	// 10.969 mV, strain gauge output at full range point 500 pound
+#define FORCE_STRAIN_AMPLIFIER_GAIN 	202.6	// Defined by R23 on Execute, better would be G=250 to max range of ADC
+#define MAX_LOAD_VOLTAGE_TO_ADC			ZERO_LOAD_VOLTAGE_TO_ADC+FULL_RANGE_OUTPUT*FORCE_STRAIN_AMPLIFIER_GAIN //2.5V+2222.3194 mV = 2.5+2.2223194 V=4.7223194 V, stain gauge full output voltage augmented by the amplifer
+#define ADC_RESOLUTION_BIT				16		//ADC resolution in bits.
+#define ADC_RESOLUTION					(uint16)pow(2,ADC_RESOLUTION_BIT) // 65535, ADC resolution in decimal.
+#define ADC_MAX_TICKS					(uint16)pow(2,ADC_RESOLUTION_BIT) // 65535, ADC maximum output, corresponding to 5 V voltage input to ADC under this circumstance
+#define ADC_MAX_TICKS_VOLTAGE			5	//V, the ADC can collect voltage up to 5 V, corresponding to 65535 ticks output output from ADC under this circumstance
+#define ZERO_LOAD_TICKS					33048  // ticks when no load applied on the load cell, less than 33054 when compressed, greater than 33054 when tensioned
+#define ZERO_LOAD_VOLTAGE_TO_ADC		2.5		// V, stain gauge voltage converted to ADC after amplifier augmentation
+#define MAX_LOAD_TICKS					(uint16)((ADC_MAX_TICKS-ZERO_LOAD_TICKS)*(MAX_LOAD_VOLTAGE_TO_ADC-ZERO_LOAD_VOLTAGE_TO_ADC)/(ADC_MAX_TICKS_VOLTAGE-ZERO_LOAD_VOLTAGE_TO_ADC))
+										// 28879 ticks, corresponding to 500 lb load
+#define FORCE_PER_TICK					FORCE_MAX/MAX_LOAD_TICKS // newton/tick, 0.0769785825165379
+
+#define FORCE_CALIB_M					0.074306 // Force = M*tick + B, from collected data set, applied load
+#define FORCE_CALIB_B					-2455.6817859 	// Force = M*tick + B, , from collected data set, applied load
+										// r^2 = 0.999954358260752
+
+#elif ACTIVE_LEG == RIGHT_ANKLE
+
+#define LOAD_MAX_IN_POUND				500		// lb, maximum mass the force sensor can measure
+#define KILOGRAM_PER_POUNUD				0.4535924
+#define LOAD_MAX_IN_KILOGRAM			LOAD_MAX_IN_POUND*KILOGRAM_PER_POUNUD		// 226.7962 kg, maximum mass the force sensor can measure
+#define GRAVITY_ACC						9.8
+#define FORCE_MAX						LOAD_MAX_IN_KILOGRAM*GRAVITY_ACC		// 2222.602613 Newton,  for LCM200 load cell maximum force the force sensor can measure
+#define FORCE_STRAIN_SENSITIVITY		2.1938 	// mV/V, force sensor sensitivity
+#define FORCE_EXCIT						5		// V, Excitation Voltage
+#define FULL_RANGE_OUTPUT				FORCE_STRAIN_SENSITIVITY*FORCE_EXCIT	// 10.969 mV, strain gauge output at full range point 500 pound
+#define FORCE_STRAIN_AMPLIFIER_GAIN 	202.6	// Defined by R23 on Execute, better would be G=250 to max range of ADC
+#define MAX_LOAD_VOLTAGE_TO_ADC			ZERO_LOAD_VOLTAGE_TO_ADC+FULL_RANGE_OUTPUT*FORCE_STRAIN_AMPLIFIER_GAIN //2.5V+2222.3194 mV = 2.5+2.2223194 V=4.7223194 V, stain gauge full output voltage augmented by the amplifer
+#define ADC_RESOLUTION_BIT				16		//ADC resolution in bits.
+#define ADC_RESOLUTION					(uint16)pow(2,ADC_RESOLUTION_BIT) // 65535, ADC resolution in decimal.
+#define ADC_MAX_TICKS					(uint16)pow(2,ADC_RESOLUTION_BIT) // 65535, ADC maximum output, corresponding to 5 V voltage input to ADC under this circumstance
+#define ADC_MAX_TICKS_VOLTAGE			5	//V, the ADC can collect voltage up to 5 V, corresponding to 65535 ticks output output from ADC under this circumstance
+#define ZERO_LOAD_TICKS					33048  // ticks when no load applied on the load cell, less than 33054 when compressed, greater than 33054 when tensioned
+#define ZERO_LOAD_VOLTAGE_TO_ADC		2.5		// V, stain gauge voltage converted to ADC after amplifier augmentation
+#define MAX_LOAD_TICKS					(uint16)((ADC_MAX_TICKS-ZERO_LOAD_TICKS)*(MAX_LOAD_VOLTAGE_TO_ADC-ZERO_LOAD_VOLTAGE_TO_ADC)/(ADC_MAX_TICKS_VOLTAGE-ZERO_LOAD_VOLTAGE_TO_ADC))
+										// 28879 ticks, corresponding to 500 lb load
+#define FORCE_PER_TICK					FORCE_MAX/MAX_LOAD_TICKS // newton/tick, 0.0769785825165379
+
+#define FORCE_CALIB_M					0.074306 // Force = M*tick + B, from collected data set, applied load
+#define FORCE_CALIB_B					-2455.6817859 	// Force = M*tick + B, , from collected data set, applied load
+										// r^2 = 0.999954358260752
+
+#endif //ACTIVE_LEG == LEFT_ANKLE
+
+
+
+//****************************************************************************
+// Running exoskeleton parameter(s)
+//****************************************************************************
+#define MOMENT_ARM_ON_FOOT 				0.25 //m, cable tension force's applied arm
+#define ANKLE_TORQUE_PER_TICK			FORCE_PER_TICK*MOMENT_ARM_ON_FOOT // N.m/tick, 0.01924465
+#define ANKLE_TORQUE_CALIB_M			0.0185766 // Torque = M*tick + B, from collected data set, applied load
+#define ANKLE_TORQUE_CALIB_B			-613.9204465 	// Torque = M*tick + B, , from collected data set, applied load
+										// r^2 = 0.999954358260752
+
 
 
 //****************************************************************************
