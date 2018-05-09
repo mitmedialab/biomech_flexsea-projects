9c7f40e660d11e3fa569f7ee1782c25cead8755d
cybanical <cybanical@gmail.com>
2018-05-09 12:19:17 -0400

    working impedance, fixed a poor loadcell connector

--- MIT_DLEG/inc/user-mn-MIT-DLeg-2dof.h
+++ MIT_DLEG/inc/user-mn-MIT-DLeg-2dof.h
@@ -111,8 +111,8 @@ void torqueSweepTest(struct act_s *actx);
 //2. Select device
 //#define DEVICE_TF08_A01			// Define specific actuator configuration. Ankle 01
 //#define DEVICE_TF08_A02		// Define specific actuator configuration. Ankle 02
-//#define DEVICE_TF08_A03		// Define specific actuator configuration. Knee 01
-#define DEVICE_TF08_A04		// Define specific actuator configuration. Knee 02
+#define DEVICE_TF08_A03		// Define specific actuator configuration. Knee 01
+//#define DEVICE_TF08_A04		// Define specific actuator configuration. Knee 02
 
 
 //Begin device specific configurations
