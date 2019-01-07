2c8b5b1ffe5e787c4872f27d49bb2dbcb5e78b5d
tshu <tshu6@gatech.edu>
2019-01-06 21:27:08 -0500

    moved config files to biomech-config submodule

--- MIT_DLEG/src/user-mn-MIT-DLeg.c
+++ MIT_DLEG/src/user-mn-MIT-DLeg.c
@@ -67,6 +67,19 @@ extern uint8_t calibrationFlags, calibrationNew;
 
 void init_MIT_DLeg(void) {
 
+	//Joint Type: activate one of these for joint limit angles.
+	//measured from nominal joint configuration, in degrees
+
+	//1. Select joint type
+	#define IS_ANKLE
+	//#define IS_KNEE
+
+	//2. Select device
+	//#define DEVICE_TF08_A01			// Define specific actuator configuration. Ankle 01
+	#define DEVICE_TF08_A02		// Define specific actuator configuration. Ankle 02
+	//#define DEVICE_TF08_A03		// Define specific actuator configuration. Knee 01
+	//#define DEVICE_TF08_A04		// Define specific actuator configuration. Knee 02
+
 
 }
 
