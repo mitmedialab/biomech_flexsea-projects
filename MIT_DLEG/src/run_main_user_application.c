410a42bce0b2334fcbb072f4a015d788ecb37d90
Matt Carney <mcarney@media.mit.edu>
2019-01-17 18:22:11 -0500

    including additional functions for user applications

---
+++ MIT_DLEG/src/run_main_user_application.c
@@ -0,0 +1,14 @@
+/*
+ * run_main_user_application.c
+ *
+ *  Created on: Jan 17, 2019
+ *      Author: matt
+ */
+
+
+#include "run_main_user_application.h"
+
+
+void run_main_user_application(Act_s *actx){
+
+}
