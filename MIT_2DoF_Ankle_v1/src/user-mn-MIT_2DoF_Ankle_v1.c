21aa7c5d17b417d5a223186c8e54fa0d0277d720
syeon <syeon@mit.edu>
2019-02-16 16:47:07 -0500

    in the process of system IDE. execute 2 does not give a sensor update

--- MIT_2DoF_Ankle_v1/src/user-mn-MIT_2DoF_Ankle_v1.c
+++ MIT_2DoF_Ankle_v1/src/user-mn-MIT_2DoF_Ankle_v1.c
@@ -59,6 +59,8 @@ int32_t mot_angs_1[6] = {0,0,0,0,0,0};
 int32_t ank_angs_2[6] = {0,0,0,0,0,0};
 int32_t mot_angs_2[6] = {0,0,0,0,0,0};
 int32_t ank_angs_t[6] = {0,0,0,0,0,0};
+int32_t ank_angs_dp[6] = {0,0,0,0,0,0};
+int32_t ank_angs_ie[6] = {0,0,0,0,0,0};
 
 int32_t ankle_vel_t = 0, ankle_torque_t = 0, last_ankle_torque_t = 0;
 int32_t angle_zero = 0, init_angle, last_DP_torque = 0;
@@ -417,6 +419,9 @@ static void ankle_2dof_refresh_values(void)
 
 	user_data_1.r[0] = ank_angs_1[0];
 	user_data_1.r[1] = ank_angs_2[0];
+
+	ank_angs_ie[0]  = (ank_angs_1[0]-ank_angs_2[0])/2;
+	ank_angs_dp[0]  = (ank_angs_1[0]+ank_angs_2[0])/2;
 }
 
 //returns the ankle angle [deg x 100]
@@ -474,24 +479,24 @@ void ankle_2dof_independent_demo(void)
 {
 	static uint8_t spinCycles = 5;
 	static uint32_t time = 0, state_t = 0;
-
+	uint8_t info[2] = {PORT_RS485_1, PORT_RS485_1};
 	//Increment time (1 tick = 1ms)
 	time++;
 	state_t++;
 
 	//State machine:
 	user_data_1.r[5] = state;
+
 	switch(state)
 	{
 		case -5:	//Wait for 5 seconds to let everything load
 
 			my_control = CTRL_OPEN;
-			user_data_1.r[2] = 3250;
-			user_data_1.r[3] = 3252;
+
 			my_pwm[0] = 0;
 			my_pwm[1] = 0;
 			init_angle = (*exec1.enc_ang);
-			if (state_t > 10000)
+			if (state_t > 7000)
 			{
 				state_t = -1;
 				state = -4;
@@ -504,19 +509,10 @@ void ankle_2dof_independent_demo(void)
 
 			my_control = CTRL_OPEN;
 
-			if(user_data_1.w[0] ==1)
-			{
-				my_pwm[0] = -user_data_1.w[1];
-				my_pwm[1] = -user_data_1.w[1];
-			}
-			else
-			{
-				my_pwm[0] = -1400;
-				my_pwm[1] = -1400;
-			}
+			my_pwm[0] = -1300; // 1.3V
+			my_pwm[1] = -1300;
 
-			user_data_1.r[4] = my_pwm[0];
-			if (state_t>3000)
+			if (state_t> 8000)
 			{
 				state = -3;
 				state_t = -1;
@@ -530,76 +526,271 @@ void ankle_2dof_independent_demo(void)
 
 			my_control = CTRL_OPEN;
 
-			if(user_data_1.w[0] ==1)
-			{
-				my_pwm[0] = user_data_1.w[1];
-				my_pwm[1] = user_data_1.w[1];
-			}
-			else
-			{
-				my_pwm[0] = 1400;
-				my_pwm[1] = 1400;
-			}
+			my_pwm[0] = 1200;
+			my_pwm[1] = 1200;
 
-			user_data_1.r[4] = my_pwm[0];
-
-			if (state_t>3000)
+			if (ank_angs_1[0]<REST_MOTOR_ANGLE)
 			{
-				state = -4;
+				my_pwm[0] = 0;
+			}
+			if (ank_angs_2[0]<REST_MOTOR_ANGLE)
+			{
+				my_pwm[1] = 0;
+			}
+			if( state_t > 10000)
+			{
+				my_pwm[0]= 0;
+				my_pwm[1]= 0;
+
+			}
+			if (ank_angs_1[0]<REST_MOTOR_ANGLE && ank_angs_2[0]<REST_MOTOR_ANGLE)
+			{
+				// state = -2;
+				state = 1;
 				state_t = -1;
-				angle_zero_1 = *exec1.enc_ang; 	//exec1.enc_display;
-				angle_zero_2 = *exec2.enc_ang;	//exec2.enc_display;
 			}
+			user_data_1.r[2] =REST_MOTOR_ANGLE;
+			user_data_1.r[3] =  exec1.gyro.x;//ank_angs_dp[0]*100;
+			user_data_1.r[4] = exec2.gyro.x;
 
+			break;
 
-			/*
-		case -4:	//Spin one motor CW...
-
-			my_control = CTRL_OPEN;
-			my_pwm[0] = 1500;
-			my_pwm[1] = 1500;
-
-			if (state_t > 1000)
+		case -2 :
+			if(state_t ==0)
 			{
+				my_control = CTRL_CURRENT;
+
+				info[0] = PORT_RS485_1;
+				tx_cmd_ctrl_mode_w(TX_N_DEFAULT, CTRL_CURRENT);
+				packAndSend(P_AND_S_DEFAULT, FLEXSEA_EXECUTE_1, info, SEND_TO_SLAVE);
+
+				info[0] = PORT_RS485_2;
+				tx_cmd_ctrl_mode_w(TX_N_DEFAULT, CTRL_CURRENT);
+				packAndSend(P_AND_S_DEFAULT, FLEXSEA_EXECUTE_2, info, SEND_TO_SLAVE);
+				/* Understanding :
+					- change mode of the control to current control for both execute
+				*/
+			}
+			else if(state_t > 10)
+			{
+				state = -1;
 				state_t = -1;
-				state = -3;
 			}
 
 			break;
 
-		case -3:	//Then CCW...
-
-			my_control = CTRL_OPEN;
-			my_pwm[0] = -1500;
-			my_pwm[1] = -1500;
-
-			if (state_t > 1000)
+		case -1 :
+			if(state_t ==0)
 			{
-				spinCycles--;
-				if(!spinCycles)
+				info[0] = PORT_RS485_1;
+				tx_cmd_ctrl_i_g_w(TX_N_DEFAULT, 30, 0, 0);
+				packAndSend(P_AND_S_DEFAULT, FLEXSEA_EXECUTE_1, info, SEND_TO_SLAVE);
+
+				info[0] = PORT_RS485_2;
+				tx_cmd_ctrl_i_g_w(TX_N_DEFAULT, 30, 0, 0);
+				packAndSend(P_AND_S_DEFAULT, FLEXSEA_EXECUTE_2, info, SEND_TO_SLAVE);
+				/*
+					write current control gain of
+					kp =30, ki = 0, kd = 0
+					Q-JF : for control gain change since then
+				*/
+			}
+			else if(state_t > 100)
+			{
+				state = 0;
+				state_t = -1;
+			}
+
+		case 0:
+			state = 1;
+			state_t = -1;
+			break;
+
+		case 1:
+			my_control = CTRL_OPEN;
+
+			my_pwm[0] = 0;
+			my_pwm[1] = 0;
+			user_data_1.r[3] =  *exec1.enc_ang;//ank_angs_dp[0]*100;
+			user_data_1.r[4] =  *exec2.enc_ang;//ank_angs_ie[0]*100;
+			break;
+		case 2:
+			/*
+			my_control = CTRL_CURRENT;
+			my_pwm[0] = 0;
+			my_pwm[1] = 0;
+
+			DP_error = (PFDF_state[0]*100-(REST_MOTOR_ANGLE-ank_angs_dp[0])); // if error is positive, ankle should try to dorsiflex
+			IE_error = (INEV_state[0]*100*LR_flip-ank_angs_ie[0]); // if error is positive, ankle should try to invert
+
+
+			dp_stiffness = (10 + PFDF_state[2]);
+			ie_stiffness = (10 + INEV_state[2]);
+
+			if (ISON_STIM && !VIRTUALFIELD)
+			{
+				dp_stiffness = dp_stiffness * STIMSTIFFGAIN;
+				ie_stiffness = ie_stiffness * STIMSTIFFGAIN;
+			}
+
+			DP_torque = DP_error*dp_stiffness; // pos torque is dorsiflexion
+			IE_torque = IE_error*ie_stiffness; // pos torque is inversion (for left foot)
+
+			if (VIRTUALFIELD)
+			{
+				DP_torqueAdd =  DFDAMPING * ank_vel_t;
+				IE_add_mult = 1;//-(pow((ank_angs_dp[0]-1000),2))/1000000;
+				if (IE_add_mult < 0)
 				{
-					state_t = -1;
-					state = -2;
+					IE_add_mult = 0;
+				}
+				if (abs(ank_angs_ie[0]) > SAFEZONE)
+				{
+					IE_torqueAdd = IESPRING * ank_angs_ie[0] * IE_add_mult;
 				}
 				else
 				{
-					state_t = -1;
-					state = -4;
+					IE_torqueAdd = 0;
 				}
+
 			}
 
+			torque_set_1 = (DP_torque + DP_torqueAdd) - (IE_torque + IE_torqueAdd);
+			torque_set_2 = (DP_torque + DP_torqueAdd) + (IE_torque + IE_torqueAdd);
+
+			if( ank_angs_1[0] > 2400)
+			{
+				torque_set_1 = torque_set_1 + k_lim_rob*(ank_angs_1[0]-2400);
+			}
+			else if( ank_angs_1[0] < 200)
+			{
+				torque_set_1 = torque_set_1 + k_lim_rob*(ank_angs_1[0]-200);
+			}
+
+			if( ank_angs_2[0] > 2400)
+			{
+				torque_set_2 = torque_set_2 + k_lim_rob*(ank_angs_2[0]-2400);
+			}
+			else if( ank_angs_2[0] < 200)
+			{
+				torque_set_2 = torque_set_2 + k_lim_rob*(ank_angs_2[0]-200);
+			}
+
+
+			set_ankle_torque_1(torque_set_1);
+			set_ankle_torque_2(torque_set_2);
+
+			DP_torque_meas = (exec1.current + exec2.current - DP_torqueAdd);// (exec1.current - exec2.current)/2.0;
+			IE_torque_meas = (exec1.current - exec2.current + IE_torqueAdd*4);
+
+			if ((BLANKWINDOW > 0 && STIMSTIFFGAIN>3) || BLANKWINDOW > 2)
+				if ((ank_angs_1[0] > 2200 || ank_angs_2[0] > 2200) && DP_torque_meas > 0)
+				{
+					DP_torque_meas = 0;
+				}
+				else if ((ank_angs_1[0] < 400 || ank_angs_2[0] < 400) && DP_torque_meas < 0)
+				{
+					DP_torque_meas = 0;
+				}
+				else if (ank_angs_ie[0] < -500 && IE_torque_meas < 0)
+				{
+					IE_torque_meas = 0;
+				}
+				else if (ank_angs_ie[0] > 500 && IE_torque_meas > 0)
+				{
+					IE_torque_meas = 0;
+				}
+
+
+			//SET STIM DATA
+			if (ISON_STIM)
+			{
+				if (DP_torque_meas >= DPSTIM_ONTHRESH)
+				{
+					isnot_stim[0] = 0;
+					DP_torque_trans = DP_torque_meas; //DP_torque - DPSTIM_ONTHRESH;
+					DP_stim_counter = 1;
+				}
+				else if (DP_torque_meas <= -DPSTIM_ONTHRESH)
+				{
+					isnot_stim[2] = 0;
+					DP_torque_trans = DP_torque_meas;
+					DP_stim_counter = 1;
+				}
+				else if (DP_stim_counter > 0 && DP_stim_counter < BLANKWINDOW)
+				{
+					DP_stim_counter += 1;
+					DP_torque_trans = 0;
+				}
+				else
+				{
+					isnot_stim[0] = 1;
+					isnot_stim[2] = 1;
+					DP_torque_trans = 0;
+					DP_stim_counter = 0;
+				}
+
+
+				if (IE_torque_meas >= IESTIM_ONTHRESH)
+				{
+					isnot_stim[3] = 0;
+					IE_torque_trans = IE_torque_meas;
+					IE_stim_counter = 1;
+				}
+				else if (IE_torque_meas <= -IESTIM_ONTHRESH)
+				{
+					isnot_stim[1] = 0;
+					IE_torque_trans = IE_torque_meas;
+					IE_stim_counter = 1;
+				}
+				else if (IE_stim_counter > 0 && IE_stim_counter < BLANKWINDOW)
+				{
+					IE_stim_counter += 1;
+					IE_torque_trans = 0;
+				}
+				else
+				{
+					isnot_stim[1] = 1;
+					isnot_stim[3] = 1;
+					IE_torque_trans = 0;
+					IE_stim_counter = 0;
+				}
+
+			}
+
+			// UPDATE THE GUI
+			if (ank_angs_dp[0] - 1000 < 0)
+			{
+				per_PFDF = (REST_MOTOR_ANGLE-ank_angs_dp[0]) *100 / (1000);
+			}
+			else
+			{
+				per_PFDF = (REST_MOTOR_ANGLE-ank_angs_dp[0]) *100 / (1500);
+			}
+
+			if (per_PFDF > 100)
+			{
+				per_PFDF = 100;
+			}
+			else if (per_PFDF < -100)
+			{
+				per_PFDF = -100;
+			}
+
+			per_INEV = -ank_angs_ie[0]*100/500;
+			if (per_INEV > 100)
+			{
+				per_INEV = 100;
+			}
+			else if (per_INEV < -100)
+			{
+				per_INEV = -100;
+			}
+
+			stiff_PFDF = PFDF_state[2] * 100 / 50;
+
+			 */
 			break;
-
-		case -2:	//Demo is over, we set everything to 0
-
-			my_control = CTRL_OPEN;
-			my_pwm[0] = 0;
-			my_pwm[1] = 0;
-			//No exit
-
-			break;
-*/
-
 
 		default:
 			//Handle exceptions here
