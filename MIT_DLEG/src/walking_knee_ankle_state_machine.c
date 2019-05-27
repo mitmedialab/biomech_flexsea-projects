1b7b0e4ca0a799e75ccccef32260bf3204ad4815
Matt Carney <cybanical@gmail.com>
2019-05-27 13:25:49 -0400

    updated for JW, stiffer ankle

--- MIT_DLEG/src/walking_knee_ankle_state_machine.c
+++ MIT_DLEG/src/walking_knee_ankle_state_machine.c
@@ -21,11 +21,11 @@ CubicSpline cubicSpline;
 
 //NOTE: All of the damping values have been reduced by 1/10 due to controller
 // Gain Parameters are modified to match our joint angle convention (RHR for right ankle, wearer's perspective). Positive Plantaflexion
-GainParams ankleGainsEst = {2.5, 0.0, 0.15, 0.0};	// may want to increase this damping, at least.
-GainParams ankleGainsMst = {2.5, 0.0, 0.15, 0.0};	// may want to increase this damping, at least.
-GainParams ankleGainsLst = {4.0, 0.0, 0.1, 10.0};
-GainParams ankleGainsEsw = {1.0, 0.0, 0.15, -5.0};
-GainParams ankleGainsLsw = {1.0, 0.0, 0.15, -5.0};
+GainParams ankleGainsEst = {3.5, 0.0, 0.15, 0.0};	// may want to increase this damping, at least.
+GainParams ankleGainsMst = {3.5, 0.0, 0.15, 0.0};	// may want to increase this damping, at least.
+GainParams ankleGainsLst = {4.0, 0.0, 0.15, 1.0};
+GainParams ankleGainsEsw = {1.0, 0.0, 0.15, 0.0};
+GainParams ankleGainsLsw = {1.0, 0.0, 0.15, 0.0};
 GainParams ankleGainsEMG = {0.0, 0.0, 0.0, 0.0};
 
 float splineTime = 100.0;
