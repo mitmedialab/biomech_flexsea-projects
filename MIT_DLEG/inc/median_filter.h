3752aba2e248f1e70e2adacd3e714d5de33b2f8a
Matt Carney <cybanical@gmail.com>
2019-11-15 16:13:17 -0500

    Median filter works, requires N/2 samples. Window size N=7 or N=9 are quite good. N=9 being a bit more robust to spikes

---
+++ MIT_DLEG/inc/median_filter.h
@@ -0,0 +1,33 @@
+/*
+ * median_filter.h
+ *
+ *  Created on: Nov 14, 2019
+ *      Author: matt
+ */
+
+#ifndef FLEXSEA_PROJECTS_MIT_DLEG_INC_MEDIAN_FILTER_H_
+#define FLEXSEA_PROJECTS_MIT_DLEG_INC_MEDIAN_FILTER_H_
+
+#include "state_variables.h"
+
+#define MEDIAN_FILTER_WINDOW_SIZE_3		3
+#define MEDIAN_FILTER_WINDOW_SIZE_5		5
+#define MEDIAN_FILTER_WINDOW_SIZE_7		7
+#define MEDIAN_FILTER_WINDOW_SIZE_9		9
+#define MEDIAN_FILTER_WINDOW_SIZE_25		25
+
+typedef float pixelvalue ;
+
+float opt_med3(float * p);
+float opt_med5(float * p);
+float opt_med7(float * p);
+float opt_med9(float * p);
+float opt_med25(float * p);
+
+float medianFilterData3( float *inputData, Act_s * actx );
+float medianFilterData5( float *inputData, Act_s * actx );
+float medianFilterData7( float *inputData, Act_s * actx );
+float medianFilterData9( float *inputData, Act_s * actx );
+float medianFilterData25( float *inputData, Act_s * actx );
+
+#endif /* FLEXSEA_PROJECTS_MIT_DLEG_INC_MEDIAN_FILTER_H_ */
